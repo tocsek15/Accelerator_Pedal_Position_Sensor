5e36bf435fbcf4ace6289e367e371019b3ddaa8a
Zoltan Torma <tocsek15@gmail.com>
2021-09-02 15:27:26 +0200

    Buildable Eclipse project

--- src/app_main.c
+++ src/app_main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "app_common.h"
-#include "app_converter.h"
-#include "app_scp.h"
+//#include "app_common.h"
+//#include "app_converter.h"
+//#include "app_scp.h"
 
 /* Entry point of the program */
 int main(void) {
+  printf("This is the main!");
+
+#if 0
   int tempNum = CONST_STUFF;
   printf("The common is reachable because the number is: %d", tempNum);
-  printf("This is the main!");
+
   app_converter();
   scp_main();
   tempNum = app_common_adder(1, 2);
   printf("The common is reachable because the number is: %d", tempNum);
+#endif
 
   return 0;
 }
