0ee433facbbc6b521fe35d5638999a35203c8048
Zoltan Torma <tocsek15@gmail.com>
2021-09-02 12:36:39 +0200

    Configuring project

---
+++ src/IConverter/app_converter.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void app_converter(void) { printf("This is the converter!"); }
--- src/ISCP/app_scp.c
+++ src/ISCP/app_scp.c
@@ -1,3 +1,6 @@
+/* Safety Critical part */
 
+#include <stdio.h>
+#include <stdlib.h>
 
-static void scp_main(void){printf("This is the SCP!")};
+static void scp_main(void) { printf("This is the SCP!"); };
