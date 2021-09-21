c52852d180cea405c2d8b400c527e61975d5c841
Zoltan Torma <tocsek15@gmail.com>
2021-09-21 14:59:33 +0200

    Updated CMake command

--- build_main.bat
+++ build_main.bat
@@ -5,7 +5,7 @@
 cmake --version
 @echo ::::::: Building has started! :::::::::::::::::::::
 @echo off
-cmake -G "MinGW Make" -S src -B "build/src" -C "build/src" 
+cmake -S src -B "build/src"
 @echo ::::::::::::::::::::::::: RUN :::::::::::::::::::::
 @echo off
 cd ./build/src
